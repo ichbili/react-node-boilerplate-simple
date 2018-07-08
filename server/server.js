162c89feffcce5b72eb75382e3fd6cf423896cab
ichbili <m.alachbili@gmail.com>
2018-07-08 13:04:40 +0100

    this one 2

--- server/server.js
+++ server/server.js
@@ -10,12 +10,16 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build/static/index.html'));
+  });
+
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build/static')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build/static', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build/static/index.html'));
     });
 }
 
