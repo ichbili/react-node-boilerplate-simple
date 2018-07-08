73cb300e6c9bc2d8dca690473baa75ac118d4b9a
ichbili <m.alachbili@gmail.com>
2018-07-08 13:38:15 +0100

    this one 5

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const path = require('path');
 
-console.log('Server started');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'client/build/index.html'));
-});
-
+// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    // app.use(express.static(path.join(__dirname, 'client/build')));
-    // // Handle React routing, return all requests to React app
-    // app.get('*', function(req, res) {
-    //   res.sendFile(path.join(__dirname, 'client/build/index.html'));
-    // });
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
