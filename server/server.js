c5f85cbdbb76bd62f3d0be7054065542db82f48e
ichbili <m.alachbili@gmail.com>
2018-07-08 13:13:45 +0100

    this one 3

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ console.log('Server started');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('*', function(req, res) {
+    res.send({ something : 'Hello my Friend'});
+  });
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build/static/index.html'));
-  });
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
